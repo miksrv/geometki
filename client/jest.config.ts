dcb38e443ebc58d13b62dd6ad18c8fe49cf15f76
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2026-03-28 09:35:09 -0700

    Add testing deps and update Jest config
    Add @testing-library/dom, identity-obj-proxy and related types to support DOM testing and CSS module mocking. Update client/jest.config.ts to expand coverage exclusions (ignore common non-test TS entry files and configs) and map the ESM-only simple-react-ui-kit to a manual CJS mock. Package changes applied and lockfile updated after installing new deps.

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -5,10 +5,18 @@ const config: Config.InitialOptions = {
     collectCoverageFrom: [
         '**/*.{js,jsx,ts,tsx}',
         '!**/*.d.ts',
+        '!**/index.ts',
+        '!**/types.ts',
+        '!**/*.types.ts',
+        '!**/middleware.ts',
+        '!**/update.ts',
+        '!**/store.ts',
+        '!**/env.ts',
         '!**/node_modules/**',
         '!<rootDir>/out/**',
         '!<rootDir>/.next/**',
         '!<rootDir>/*.config.js',
+        '!<rootDir>/*.config.ts',
         '!<rootDir>/coverage/**'
     ],
     coverageProvider: 'v8',
@@ -23,6 +31,9 @@ const config: Config.InitialOptions = {
         // Handle CSS imports (with CSS modules)
         // https://jestjs.io/docs/webpack#mocking-css-modules
         '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+
+        // Map ESM-only packages to their manual CJS mocks
+        '^simple-react-ui-kit$': '<rootDir>/__mocks__/simple-react-ui-kit.tsx',
 
         // Handle module aliases
         '^@/(.*)$': '<rootDir>/$1',
