f82701e01d64bcd106e6394f92a06017b7cb9911
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2026-04-07 19:16:46 -0700

    Fixed ESLint and Prettier issues

--- client/pages/unsubscribe.tsx
+++ client/pages/unsubscribe.tsx
@@ -20,20 +20,30 @@ const UnsubscribePage: NextPage<object> = () => {
     const router = useRouter()
     const searchParams = useSearchParams()
 
-    const mailId   = searchParams?.get('mail')
+    const mailId = searchParams?.get('mail')
     const digestId = searchParams?.get('digest')
 
-    const { data: mailData, error: mailError, isLoading: mailLoading, isSuccess: mailSuccess, isError: mailIsError } =
-        API.useMailGetUnsubscribeQuery(mailId || '', { skip: !mailId })
+    const {
+        data: mailData,
+        error: mailError,
+        isLoading: mailLoading,
+        isSuccess: mailSuccess,
+        isError: mailIsError
+    } = API.useMailGetUnsubscribeQuery(mailId || '', { skip: !mailId })
 
-    const { data: digestData, error: digestError, isLoading: digestLoading, isSuccess: digestSuccess, isError: digestIsError } =
-        API.useMailGetUnsubscribeDigestQuery(digestId || '', { skip: !digestId })
+    const {
+        data: digestData,
+        error: digestError,
+        isLoading: digestLoading,
+        isSuccess: digestSuccess,
+        isError: digestIsError
+    } = API.useMailGetUnsubscribeDigestQuery(digestId || '', { skip: !digestId })
 
-    const data      = mailData    ?? digestData
-    const error     = mailError   ?? digestError
+    const data = mailData ?? digestData
+    const error = mailError ?? digestError
     const isLoading = mailLoading || digestLoading
     const isSuccess = mailSuccess || digestSuccess
-    const isError   = mailIsError || digestIsError
+    const isError = mailIsError || digestIsError
 
     useEffect(() => {
         if (!mailId && !digestId) {
--- client/sections/user/user-form/UserForm.tsx
+++ client/sections/user/user-form/UserForm.tsx
@@ -162,17 +162,19 @@ export const UserForm: React.FC<UserFormProps> = ({ loading, values, errors, onS
 
             <div className={styles.section}>
                 <h3 className={styles.header}>{t('sending-notifications-by-email')}</h3>
-                {['emailPhoto', 'emailRating', 'emailComment', 'emailEdit', 'emailCover', 'emailDigest'].map((setting) => (
-                    <Checkbox
-                        className={styles.settings}
-                        key={setting}
-                        id={setting}
-                        label={t(`checkbox_${setting}`)}
-                        disabled={loading}
-                        onChange={handleChangeCheckbox}
-                        checked={formData?.settings?.[setting as keyof ApiModel.UserSettings]}
-                    />
-                ))}
+                {['emailPhoto', 'emailRating', 'emailComment', 'emailEdit', 'emailCover', 'emailDigest'].map(
+                    (setting) => (
+                        <Checkbox
+                            className={styles.settings}
+                            key={setting}
+                            id={setting}
+                            label={t(`checkbox_${setting}`)}
+                            disabled={loading}
+                            onChange={handleChangeCheckbox}
+                            checked={formData?.settings?.[setting as keyof ApiModel.UserSettings]}
+                        />
+                    )
+                )}
             </div>
 
             {!loading && !!values?.authType && (
@@ -275,12 +277,12 @@ const mapFormValues = (values?: FormDataType): FormDataType => ({
     oldPassword: '',
     settings: {
         emailComment: values?.settings?.emailComment ?? true,
-        emailCover:   values?.settings?.emailCover   ?? true,
-        emailDigest:  values?.settings?.emailDigest  ?? true,
-        emailEdit:    values?.settings?.emailEdit    ?? true,
-        emailPhoto:   values?.settings?.emailPhoto   ?? true,
-        emailPlace:   values?.settings?.emailPlace   ?? true,
-        emailRating:  values?.settings?.emailRating  ?? true
+        emailCover: values?.settings?.emailCover ?? true,
+        emailDigest: values?.settings?.emailDigest ?? true,
+        emailEdit: values?.settings?.emailEdit ?? true,
+        emailPhoto: values?.settings?.emailPhoto ?? true,
+        emailPlace: values?.settings?.emailPlace ?? true,
+        emailRating: values?.settings?.emailRating ?? true
     },
     website: values?.website
 })
